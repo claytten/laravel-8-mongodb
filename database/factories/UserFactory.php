487eee4cbd6e8e2b15ce6bc344765e21af805db1
aji <wahyuajisulaiman@gmail.com>
2023-05-09 17:31:06 +0700

    update address use for  sale vehicle (test includes)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => $this->faker->optional()->dateTimeThisCentury(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'address' => $this->faker->address(),
             'remember_token' => Str::random(10),
         ];
     }
